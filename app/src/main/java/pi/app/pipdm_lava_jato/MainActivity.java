2263fa65d26b63218f61eed76e00afac140e9571
SAMUEL SILVA <samuel.brazz@gmail.com>
2024-06-19 18:41:10 -0300

    Primeiro commit

---
+++ app/src/main/java/pi/app/pipdm_lava_jato/MainActivity.java
@@ -0,0 +1,14 @@
+package pi.app.pipdm_lava_jato;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
