cca7a0dd85b09e69eac29bd26c9019be2d04ca67
SAMUEL SILVA <samuel.brazz@gmail.com>
2024-06-21 02:16:18 -0300

    Inclusão do front

---
+++ app/src/main/java/lava_jato/app/view/LoginActivity.java
@@ -0,0 +1,59 @@
+package lava_jato.app.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import pi.app.pipdm_lava_jato.R;
+
+import lava_jato.app.dao.Maindao;
+import lava_jato.app.viewmodel.UserViewModel;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailEditText, senhaEditText;
+    private Maindao mainDAO;
+    private UserViewModel userViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailEditText = findViewById(R.id.te_Login_Email);
+        senhaEditText = findViewById(R.id.te_Login_Senha);
+
+        mainDAO = new Maindao(this);
+        userViewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(UserViewModel.class);
+    }
+
+    public void btn_onLoginClick(View view) {
+        String email = emailEditText.getText().toString();
+        String senha = senhaEditText.getText().toString();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(senha)) {
+            Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        userViewModel.getUsuarioLiveData(email).observe(this, usuario -> {
+            if (usuario != null && userViewModel.authenticateUser(email, senha)) {
+                Toast.makeText(this, "Login bem-sucedido!", Toast.LENGTH_SHORT).show();
+                setContentView(R.layout.activity_agendamento);
+            } else {
+                Toast.makeText(this, "Email ou senha incorretos!", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        public void btn_redirectToRegister(View view) {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/lava_jato/app/view/RegisterActivity.java
@@ -0,0 +1,64 @@
+package lava_jato.app.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import pi.app.pipdm_lava_jato.R;
+
+import lava_jato.app.viewmodel.UserViewModel;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText nomeEditText, senhaEditText, emailEditText, telefoneEditText, cpfEditText;
+    private UserViewModel userViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        nomeEditText = findViewById(R.id.te_Cadastro_Nome);
+        senhaEditText = findViewById(R.id.te_Cadastro_Senha);
+        emailEditText = findViewById(R.id.te_Cadastro_Email);
+        telefoneEditText = findViewById(R.id.te_Cadastro_Telefone);
+        cpfEditText = findViewById(R.id.te_Cadastro_CPF);
+
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+
+        userViewModel.getUsuarioLiveData(emailEditText.getText().toString()).observe(this, usuario -> {
+            if (usuario != null) {
+                Toast.makeText(this, "UsuÃ¡rio registrado com sucesso!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+
+    public void btn_onRegisterClick(View view) {
+        String nome = nomeEditText.getText().toString();
+        String senha = senhaEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+        String telefone = telefoneEditText.getText().toString();
+        String cpf = cpfEditText.getText().toString();
+
+        if (TextUtils.isEmpty(nome) || TextUtils.isEmpty(senha) || TextUtils.isEmpty(email) ||
+                TextUtils.isEmpty(telefone) || TextUtils.isEmpty(cpf)) {
+            Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        userViewModel.registerUser(nome, email, senha, telefone, cpf);
+
+        public void redirectToLogin(View view){
+            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+}
